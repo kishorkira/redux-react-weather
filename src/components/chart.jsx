08524c63be1d48574a03b306e4b306cfb8781913
kishorkira <kishorkira@gmail.com>
2018-11-02 21:44:42 +0530

    Add: more info to charts

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import React from "react";
+import _ from "lodash";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from "react-sparklines";
 
-export default function(props){
-    return(
-        <React.Fragment>
-            <Sparklines data={props.data} height={120} width={180}>
-                <SparklinesLine color={props.color} />
-            </Sparklines>
-        </React.Fragment>  
-
-    );
-} 
+function avrage(data) {
+  return _.round(_.sum(data) / data.length);
+}
+export default function(props) {
+  return (
+    <React.Fragment>
+      <Sparklines data={props.data} height={80} width={180}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>
+        {avrage(props.data)} {props.units}
+      </div>
+    </React.Fragment>
+  );
+}
--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import Chart from '../components/chart';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Chart from "../components/chart";
 
 class WeatherList extends Component {
-    renderWeather=(cityData)=>{
-        const name = cityData.city.name;
-        const temparature = cityData.list.map(weather=> weather.main.temp);
-        const hunidity = cityData.list.map(weather=> weather.main.humidity);
-        const pressure = cityData.list.map(weather=> weather.main.pressure);
-        
-        return(
-            <tr key={name}>
-                <td>
-                    {name}
-                </td>
-                <td>
-                    <Chart data={temparature} color="red" />
-                </td>
-                <td>
-                   
-                </td>
-                <td>
-                   
-                </td>
-            </tr>
-        );
-    };
-    render() { 
-        return (        
-           <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temparature</th>
-                        <th>Humidity</th>
-                        <th>Pressure</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
-            
-         );
-    }
+  renderWeather = cityData => {
+    const name = cityData.city.name;
+    const temparature = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temparature} color="red" units="k" />
+        </td>
+        <td>
+          <Chart data={humidity} color="green" units="%" />
+        </td>
+        <td>
+          <Chart data={pressure} color="blue" units="hPa" />
+        </td>
+      </tr>
+    );
+  };
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temparature</th>
+            <th>Humidity</th>
+            <th>Pressure</th>
+          </tr>
+        </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+      </table>
+    );
+  }
 }
-function mapStateToProps({weather}){
-    return {
-        weather 
-    };
+function mapStateToProps({ weather }) {
+  return {
+    weather
+  };
 }
- 
+
 export default connect(mapStateToProps)(WeatherList);
